c20ccb5a7ce1a2f88be2ef4dad234d05ae286c34
derpierre65 <git@derpierre65.de>
2023-02-26 14:58:46 +0100

    fix maxValues in QSelect if maxlength is given

---
+++ ui/dev/src/pages/form/select-part-10-maxlength-maxvalues.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-layout-padding column q-gutter-y-lg">
+    <q-select
+      v-model="tags"
+      :max-values="10"
+      label="Tags"
+      new-value-mode="add-unique"
+      input-debounce="0"
+      color="white"
+      maxlength="25"
+      counter
+      use-input
+      use-chips
+      multiple
+      square
+      outlined
+      dense
+      hide-dropdown-icon
+      @new-value="addTag" />
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  setup() {
+    return {
+      tags: ref([]),
+    }
+  },
+  methods: {
+    addTag(val, done) {
+      done(val);
+    },
+  }
+}
+</script>
